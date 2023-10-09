f212e7388af4ee433fe910ac2b429880b74f0021
David Yang <d_yang19@u.pacific.edu>
2023-10-09 11:12:13 -0700

    Buttons each scene lead to designated scenes.

---
+++ Assets/Scripts/Menus.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    //Opens GameScene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/GameScene");
+    }
+
+    //Opens Controls Scene
+    public void OpenControls()
+    {
+        SceneManager.LoadScene("Scenes/ControlsMenuScene");
+    }
+
+    //Returns to the Main Menu Scene
+    public void ReturnMain()
+    {
+        SceneManager.LoadScene("Scenes/MainMenuScene");
+    }
+}
