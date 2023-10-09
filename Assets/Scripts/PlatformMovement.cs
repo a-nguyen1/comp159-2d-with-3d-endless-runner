33b1ef3bbeb97de0d8414908aef114b82bc10151
Brendon <b_amino@u.pacific.edu>
2023-10-09 13:33:27 -0700

    Added PlayerController script and PlatformMovement Script in a test scene

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    //Can be integrated with PlatformController once finished.
+
+    [SerializeField] float accel = 1f;
+    private float _speed = 0f;
+
+    private void Update()
+    {
+        _speed += accel * Time.deltaTime * 0.5f;
+        transform.position += Vector3.left * _speed * Time.deltaTime;
+        _speed += accel * Time.deltaTime * 0.5f;
+    }
+}
