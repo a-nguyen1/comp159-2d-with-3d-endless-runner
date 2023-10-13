19e992fc2ef08fbe63ba8af3e415f337de9f9961
David Yang <d_yang19@u.pacific.edu>
2023-10-13 13:55:13 -0700

    Added a delete point for the platforms

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
+    [SerializeField] private Transform platformDeletePoint;
+    private GameObject[] _platforms;
     
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,15 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _platforms = GameObject.FindGameObjectsWithTag("Platform");
         
+        foreach(GameObject platform in _platforms)
+        {
+            if (platform.transform.position.x < platformDeletePoint.position.x)
+            {
+                Destroy(platform);
+                Debug.Log("DESTROYED PLATFORM!!!");
+            }
+        }
     }
 }
