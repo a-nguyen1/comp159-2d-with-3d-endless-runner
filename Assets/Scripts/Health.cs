548690540590f3a42592e29b43fd76c5a1e5957d
Austin Nguyen <a_nguyen202@u.pacific.edu>
2023-10-13 21:58:17 -0700

    add invincibility window

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private float invincibilityWindow = 1;
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private bool invincible;
     private bool dead;
     
     private void Awake()
@@ -15,21 +17,35 @@ public class Health : MonoBehaviour
 
     public void TakeDamage(float _damage)
     {
+        if (invincible) return; // don't take damage when invincible
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-        Debug.Log($"{currentHealth}");
         if (currentHealth > 0)
         {
-          
+            StartCoroutine("Invincibility");
         }
         else
         {
             if (!dead)
-            { 
-                
-                // GetComponent<PlayerController>().enabled = false;
-                // dead = true;
+            {
+                GameObject player = GameObject.FindWithTag("Player");
+                GameObject gameController = GameObject.FindWithTag("GameController");
+                gameController.GetComponent<GameController>().GameOver();
+                Destroy(player.gameObject);
+                dead = true;
             }
 
         }
+    }
+
+    public void SetInvincibility(bool invincibility)
+    {
+        invincible = invincibility;
+    }
+
+    private IEnumerator Invincibility()
+    {
+        invincible = true;
+        yield return new WaitForSeconds(invincibilityWindow);
+        invincible = false;
     }
 }
