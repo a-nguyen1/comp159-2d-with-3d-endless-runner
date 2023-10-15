8c3e29c4c99883fe0977ac0d8a75dd700a261104
Brendon <b_amino@u.pacific.edu>
2023-10-15 16:48:50 -0700

    Added fsat falling for a full jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,30 +2,60 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     private bool isJumping;
     private Rigidbody rb;
     [SerializeField] private LayerMask whatIsGround;
-    //[SerializeField] private float maxJumpHeight;
+    [SerializeField] private float maxJumpHeight;
+    [SerializeField] private float fastFallMultiplier;
+    private bool jumpRequest;
+    private bool isHoldingJump;
+
+    private float playerMaxJumpHeight;
     //private RaycastHit info;
     // Start is called before the first frame update
     void Start()
     {
+        isHoldingJump = false;
+        jumpRequest = false;
         isJumping = false;
         rb = GetComponent<Rigidbody>();
+        playerMaxJumpHeight = transform.position.y + maxJumpHeight;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleJump();
+        JumpRequest();
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumpRequest)
+        {
+            HandleJump();
+        }
+        ScaleGravity();
+    }
+
+    private void JumpRequest()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            jumpRequest = true;
+        }
+        else
+        {
+            jumpRequest = false;
+        }
     }
 
     private void HandleJump()
     {
-        if (!isJumping && OnGround()) //Being OnGround implies that you canJump
+        if (!isJumping && IsOnGround()) //Being OnGround implies that you canJump
         {
             isJumping = true;
             Jump();
@@ -40,16 +70,33 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
-            //Add Force to RB
-            //Ideally, this will be a multi-stage jump depending on how long you hold down your jump key.
-            //Thus a parabola with differing jump heights
-            //But how easy is it to manipulate force and gravity to achieve this?
-            rb.AddForce(Vector3.up * 3,ForceMode.Impulse);
+            rb.AddForce(Vector3.up * maxJumpHeight,ForceMode.Impulse);
+            isHoldingJump = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow);
+            if (isHoldingJump)
+            {
+                if (!Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.UpArrow))
+                {
+                    isHoldingJump = false;
+                }
+            }
             ///throw new Exception("Unimplemented");
         }
     }
-    private bool OnGround()
+
+    private void ScaleGravity()
     {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += new Vector3(0, Physics.gravity.y * fastFallMultiplier * Time.deltaTime, 0);
+        } 
+        else if (rb.velocity.y > 0 && !isHoldingJump)
+        {
+            rb.velocity += new Vector3(0, Physics.gravity.y * fastFallMultiplier * 2 * Time.deltaTime, 0);
+        }
+    }
+    private bool IsOnGround()
+    {
+        playerMaxJumpHeight = transform.position.y + maxJumpHeight;
         Ray ray = new Ray(transform.position, -transform.up);
         return Physics.SphereCast(ray, 0.1f, 1f, whatIsGround);
     }
